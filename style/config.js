2089bee560968275654c6d79a864d5ad429399ce
taq0710 <96494548+taq0710@users.noreply.github.com>
2022-12-24 12:24:20 +0700

    Add files via upload

---
+++ style/config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Babee CLick here!';
+const giftUrl = 'https://www.facebook.com/loveangel2099';
+const eventName = 'Merry Christmax';
+const titleCard = 'Babee!';
+const contentCard = 'Chúc tình yêu của anh Noel vui vẻ ngập tràn niềm vui, cảm ơn vì em đã đến bên anh. Hy vọng từ nay và sau này mỗi mùa Noel đều có nhau!';
+const giftImage = './style/tinhyeu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
